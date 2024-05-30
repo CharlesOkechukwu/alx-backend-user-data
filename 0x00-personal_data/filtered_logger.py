75bc80a35302b822d7d1eaf54f4e92eca16d99b9
CharlesOkechukwu <realcharlieok@gmail.com>
2024-05-31 00:51:38 +0100

    format record in class RedactingFormatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,3 +12,22 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(fr'{field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format record and return message"""
+        return filter_datum(self.fields, self.REDACTION, super(
+            RedactingFormatter, self).format(record), self.SEPARATOR
+        )
