da1a1609249e43321aa8f714aaa86485e47166b5
CharlesOkechukwu <realcharlieok@gmail.com>
2024-06-06 00:58:40 +0100

    implement require auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,14 @@ class Auth:
     """class managing API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """check path to confirm authentication requirement"""
-        return False
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+        for route in excluded_paths:
+            if route.endswith('*') and path.startswith(route[:-1]):
+                return False
+            elif path == route:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """check the authorization header"""
