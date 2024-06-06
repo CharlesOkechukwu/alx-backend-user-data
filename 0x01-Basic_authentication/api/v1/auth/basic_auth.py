e273786cf8d05e8e35fd5e97cd192071dcfb9418
CharlesOkechukwu <realcharlieok@gmail.com>
2024-06-06 02:39:13 +0100

    impelement extract base64 authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,13 @@ from .auth import Auth
 
 class BasicAuth(Auth):
     """class to manage API basic authentication"""
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str
+                                            ) -> str:
+        """extract base64 authorization header"""
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
