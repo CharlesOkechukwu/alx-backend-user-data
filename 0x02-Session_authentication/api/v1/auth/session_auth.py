f79822ef9e6810434ca8218b219d4d4fea9a4a7c
CharlesOkechukwu <realcharlieok@gmail.com>
2024-06-06 09:24:45 +0100

    implement SessionAuth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module for implementing session auth class"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """class to manage API session authentication"""
+    pass
