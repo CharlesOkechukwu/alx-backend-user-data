b9d8295625065db92d681cb43478c3a8fff3f438
CharlesOkechukwu <realcharlieok@gmail.com>
2024-06-06 04:01:06 +0100

    implement extract user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """module to implement basic auth class"""
 from .auth import Auth
 import base64
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -30,3 +31,15 @@ class BasicAuth(Auth):
             return decoded.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str
+                                 ) -> Tuple[str]:
+        """extract and return user credentials"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        credentials = decoded_base64_authorization_header.split(':')
+        return credentials[0], ':'.join(credentials[1:])
