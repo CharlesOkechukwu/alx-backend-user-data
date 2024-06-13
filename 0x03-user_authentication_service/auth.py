9bfc1eca907abf103fc157cf299b10f308353c4a
CharlesOkechukwu <realcharlieok@gmail.com>
2024-06-14 00:12:46 +0100

    get password reset token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -71,3 +71,14 @@ class Auth:
         except ValueError:
             pass
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """get reset passowrd token"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        else:
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
