bba84e49d745e1d4f3e9ef9bd6b55eb6465af3a9
CharlesOkechukwu <realcharlieok@gmail.com>
2024-06-13 04:24:30 +0100

    create user if user don't exist

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """Flask app module"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from .auth import Auth
 
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -10,6 +13,18 @@ app = Flask(__name__)
 def root() -> str:
     """index root"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """register a user route"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": "{}".format(email), "message": "user created"})
 
 
 if __name__ == "__main__":
