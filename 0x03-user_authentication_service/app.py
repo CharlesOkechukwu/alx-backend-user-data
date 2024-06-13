1dcf784ac2c762004674a91008c430e8b02fe50a
CharlesOkechukwu <realcharlieok@gmail.com>
2024-06-13 23:40:59 +0100

    get user's profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -53,5 +53,15 @@ def logout() -> None:
     return redirect("/")
 
 
+@app.route("/profile", strict_slashes=False)
+def profile() -> None:
+    """Handle profile request"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": "{}".format(email)}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
