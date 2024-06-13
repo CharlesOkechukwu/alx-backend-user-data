03b7f9bc3bebebc62cc147c639db9a1324103bf2
CharlesOkechukwu <realcharlieok@gmail.com>
2024-06-13 23:45:29 +0100

    get user's profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -60,7 +60,7 @@ def profile() -> None:
     user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
-    return jsonify({"email": "{}".format(email)}), 200
+    return jsonify({"email": "{}".format(user.email)}), 200
 
 
 if __name__ == "__main__":
